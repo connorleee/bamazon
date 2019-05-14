9ea5d39e029e3801e2b01803509f738eaaaf2521
Connor Lee <45024235+connorleee@users.noreply.github.com>
2019-05-14 00:12:26 -0700

    basic inquiry for product and qty. update database if qty is in stock. notify user if not in stock. Need to make this recursive or terminate connection.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,15 +11,71 @@ const connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
 
     connection.query(
         "select * from products",
-        function(err, res){
+        function (err, res) {
             if (err) throw err;
-            console.log("we in this")
             console.log(res)
+
+            inquirer
+                .prompt([
+                    {
+                        name: "product",
+                        message: "What product ID would you like to purchase?",
+                        type: "rawlist",
+                        choices: function () {
+                            products = [];
+
+                            res.forEach(element => {
+                                products.push(element.product_name)
+                            });
+
+                            return products;
+                        }
+                    },
+                    {
+                        name: "purchaseQty",
+                        message: `How many would you like to purchase?`,
+                        type: "number"
+                    }
+                ])
+                .then(answers => {
+                    let chosenItem;
+                    res.forEach(el => {
+                        if (el.product_name === answers.product){
+                            chosenItem = el;
+                        }
+                    })
+
+                    let availableQty = chosenItem.stock_quantity;
+
+                    if (answers.purchaseQty > availableQty){
+                        console.log(`Sorry, but we do not have that many ${answers.product}s in stock. Please submit a new order with a quantity less than or equal to ${availableQty}`)
+                    } else {
+
+                        connection.query(
+                            "UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    stock_quantity: availableQty - answers.purchaseQty
+                                },
+                                {
+                                    product_name: answers.product
+                                }
+                            ],
+                            (err) => {
+                                if (err) throw err;
+                                console.log(`Thank you for your purchase!`)
+                            }
+                        )
+                    }
+                });
         }
-    )
+    );
+
+    // connection.end();
 })
+
